ffc2654d7aa12aef3c7a39c2b9eb1d10d4b522a1
Christopher Rene <objectivechris@gmail.com>
2017-03-13 22:02:25 -0400

    Initial Commit

---
+++ BookLovr/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BookLovr
+//
+//  Created by Christopher Rene on 3/13/17.
+//  Copyright © 2017 Christopher Rene. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
